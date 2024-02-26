c3bd5232b99ac61c9ace01c8ab3758eeae8be309
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-26 07:27:49 +0100

    colorPage

---
+++ app/colors/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import React from "react";
+
+type Props = {};
+
+
+
+const page: NextPage = (props: Props) => {
+  return <div>page</div>;
+};
+
+export default page;
